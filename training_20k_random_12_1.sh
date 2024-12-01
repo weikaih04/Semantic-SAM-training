6f79174df1c83562f5a85c3565d1649b0251bf6a
JieyuZ2 <jieyuzhang97@gmail.com>
2024-12-01 15:57:24 -0800

    add tmp

--- training_20k_random_12_1.sh
+++ training_20k_random_12_1.sh
@@ -1,5 +1,5 @@
-export SAM_JSON="/training_dataset"
-export SAM_DATASETS="/training_dataset"
+export SAM_JSON="/tmp_dataset"
+export SAM_DATASETS="/tmp_dataset"
 export DATASET="/datasets"
 n=8
 python train_net.py --resume --num-gpus $n --config-file configs/semantic_sam_only_sa-1b_swinT_our.yaml COCO.TEST.BATCH_SIZE_TOTAL=$n SAM.TEST.BATCH_SIZE_TOTAL=$n SAM.TRAIN.BATCH_SIZE_TOTAL=$n
