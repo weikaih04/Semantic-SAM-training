78e13aacfd3f77d2ec9689d9419e17fe2ff66176
JieyuZ2 <jieyuzhang97@gmail.com>
2024-12-02 08:13:21 +0000

    add new config

--- training_20k_random_12_1.sh
+++ training_20k_random_12_1.sh
@@ -1,6 +1,6 @@
-export SAM_JSON="/training_dataset"
-export SAM_DATASETS="/training_dataset"
+export SAM_JSON="/tmp_dataset"
+export SAM_DATASETS="/tmp_dataset"
 export DATASET="/datasets"
 export WANDB_API_KEY=f773908953fc7bea7008ae1cf3701284de1a0682
-n=8
-python train_net.py --resume --num-gpus $n --config-file configs/semantic_sam_only_sa-1b_swinT_our.yaml COCO.TEST.BATCH_SIZE_TOTAL=$n SAM.TEST.BATCH_SIZE_TOTAL=$n SAM.TRAIN.BATCH_SIZE_TOTAL=$n
+n=2
+python train_net.py --resume --num-gpus $n --config-file configs/semantic_sam_only_sa-1b_swinT_20k_random_12_1.yaml COCO.TEST.BATCH_SIZE_TOTAL=$n SAM.TEST.BATCH_SIZE_TOTAL=$n SAM.TRAIN.BATCH_SIZE_TOTAL=$n
